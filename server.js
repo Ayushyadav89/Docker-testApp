863d5fdc023bee342e7edc764193c507c317a277
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2025-04-16 22:02:10 +0530

    update the docker network

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 5050;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MONGO_URL = "mongodb://admin:yaduvanshi@2004@localhost:27017";
+const MONGO_URL = "mongodb://admin:ydv@localhost:27017";
 const client = new MongoClient(MONGO_URL);
 
 //GET all users
